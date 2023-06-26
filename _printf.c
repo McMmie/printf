e54e820aa936d5551cb1a11e95bdc714d3b833cb
Abdulmalik Aremu <aremzy2018@gmail.com>
2023-06-26 16:29:15 +0000

    fixed betty error

--- _printf.c
+++ _printf.c
@@ -1,10 +1,17 @@
 #include <stdarg.h>
 #include "main.h"
+/**
+ *_printf - This is a complets replica of the printf function
+ *@format: The arguements formats
+ *
+ *Return: always zero
+ */
 int _printf(const char *format, ...)
 {
 	const char *countchar;
 	char *s;
 	va_list arg;
+
 	va_start(arg, format);
 	for (countchar = format; *countchar != '\0'; countchar++)
 	{
@@ -14,14 +21,17 @@ int _printf(const char *format, ...)
 			countchar++;
 		}
 		countchar++;
-		switch(*countchar)
+		switch (*countchar)
 		{
-		case 'c': _putchar(va_arg(arg, int));
+		case 'c':
+			_putchar(va_arg(arg, int));
 			break;
-		case 's': s = va_arg(arg, char *);
+		case 's':
+			s = va_arg(arg, char *);
 			_printstr(s);
 			break;
-		case '%': _putchar('%');
+		case '%':
+			_putchar('%');
 			break;
 		}
 	}
--- test/test.c
+++ test/test.c
@@ -3,14 +3,15 @@
 int main(void)
 {
 	char alpha = 'a';
-	char string[7] = "string";
+	char string[] = "string";
 	printf("this is a string\n");
 	_printf("this is a string\n");
 	printf("%c  \n", alpha);
 	_printf("%c  \n", alpha);
 	printf("%s\n", string);
 	_printf("%s\n", string);
-	_printf("%c\n", alpha);
-	printf("%c\n", alpha);
+	_printf("%c \n", alpha);
+	printf("%c \n", alpha);
+	printf("%c \n", alpha);
 	return (0);
 }
